7cb29ee497734f523bab491dca2106026d722824
Adhiraj <adhirajb1109@gmail.com>
2021-06-26 13:28:11 +0530

    Replaces SQLite With MySQL

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root@localhost/flask-todolist"
 db = SQLAlchemy(app)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 class Database(db.Model):
