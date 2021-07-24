2d8ebc069260478ec906b6854ff70bbfc00ea2e2
Adhiraj <adhirajb1109@gmail.com>
2021-07-24 12:35:19 +0530

    commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import Form
+from wtforms import StringField , SubmitField
+from wtforms.validators import DataRequired , length
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///tasky.db"
+app.config["SECRET_KEY"] = "Secret Key"
 db = SQLAlchemy(app)
 
 
@@ -14,31 +18,40 @@ class Tasky(db.Model):
         return f"{self.sno} - {self.title}"
 
 
+class TaskForm(Form):
+    title = StringField('Enter Task Title :', validators=[DataRequired() , length(max=200)])
+    description = StringField('Enter Task Description :', validators=[DataRequired() , length(max=500)])
+    submit = SubmitField('Submit')
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == 'POST':
-        title = request.form['title']
-        description = request.form['description']
-        task = Tasky(title=title,
+        form = TaskForm()
+        if form.validate_on_submit():
+          title = request.form['title']
+          description = request.form['description']
+          task = Tasky(title=title,
                         description=description)
-        db.session.add(task)
-        db.session.commit()
+          db.session.add(task)
+          db.session.commit()
     tasks = Tasky.query.all()
-    return render_template("index.html", tasks=tasks)
+    return render_template("index.html", tasks=tasks , form=TaskForm())
 
 @app.route("/update/<int:sno>", methods=["GET", "POST"])
 def update(sno):
     if request.method == 'POST':
-        title = request.form['title']
-        description = request.form['description']
-        task = Tasky.query.filter_by(sno=sno).first()
-        task.title = title
-        task.description = description
-        db.session.add(task)
-        db.session.commit()
+        form = TaskForm()
+        if form.validate_on_submit():
+          title = request.form['title']
+          description = request.form['description']
+          task = Tasky.query.filter_by(sno=sno).first()
+          task.title = title
+          task.description = description
+          db.session.add(task)
+          db.session.commit()
         return redirect('/')
     task = Tasky.query.filter_by(sno=sno).first()
-    return render_template("update.html", task=task)
+    return render_template("update.html", task=task , form=TaskForm())
 
 
 @app.route("/delete/<int:sno>")
